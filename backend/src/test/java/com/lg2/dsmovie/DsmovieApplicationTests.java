b6ace4a84cf4ef1fb42413bdab7e1acf0f56b3ae
ASAngelica <asa196549@gmail.com>
2022-03-09 19:02:45 -0300

    Projeto criado

---
+++ backend/src/test/java/com/lg2/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lg2.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
